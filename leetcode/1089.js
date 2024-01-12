aa6a52715d8783639cf1f9b7e2b1358f4580fc82
Ananthakrishnan KP <ananthukp17@gmail.com>
2024-01-13 02:30:19 +0530

    leetcode 1089

---
+++ leetcode/1089.js
@@ -0,0 +1,11 @@
+//leetcode 1089   Duplicate zeros
+
+const arr=[1,0,2,3,0,4,5,0];
+var duplicateZeros = function(arr) {
+    for(arr =0; i<arr.length; i++){
+        arr.splice(i,0,0);
+        arr=pop()
+        i+=1
+    }
+};
+console.log(duplicateZeros(arr));
