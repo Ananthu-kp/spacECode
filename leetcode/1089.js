34947bc1d4c82134e037ec024542e369ff5ed145
Ananthakrishnan KP <ananthukp17@gmail.com>
2024-01-13 10:19:14 +0530

    leetcode 1089

--- leetcode/1089.js
+++ leetcode/1089.js
@@ -1,11 +1,14 @@
 //leetcode 1089   Duplicate zeros
 
-const arr=[1,0,2,3,0,4,5,0];
-var duplicateZeros = function(arr) {
-    for(arr =0; i<arr.length; i++){
-        arr.splice(i,0,0);
-        arr=pop()
-        i+=1
+const arr = [1, 0, 2, 3, 0, 4, 5, 0];
+var duplicateZeros = function (arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            arr.splice(i, 0, 0);
+            arr.pop()
+            i++;
+        }
     }
+    return arr
 };
 console.log(duplicateZeros(arr));
