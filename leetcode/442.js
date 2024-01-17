859054f992ccd33346da2955765ad9e57667e9f5
Ananthakrishnan KP <ananthukp17@gmail.com>
2024-01-17 17:23:25 +0530

    leetcode442

---
+++ leetcode/442.js
@@ -0,0 +1,16 @@
+//leetcode 442  find all duplicates in an array
+
+let nums = [4,3,2,7,8,2,3,1];
+
+var findDuplicates = function(nums) {
+    let result=[];
+    nums.sort();
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===nums[i+1]){
+            result.push(nums[i])
+        }
+    }
+    return result.sort();
+}
+
+console.log(findDuplicates(nums));
