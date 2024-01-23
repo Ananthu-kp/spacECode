f9dcc64b5dcd5bc2aa124a60efe30dce01582cd6
Ananthakrishnan KP <ananthukp17@gmail.com>
2024-01-23 17:31:50 +0530

    leetcode 283

--- leetcode/283.js
+++ leetcode/283.js
@@ -8,19 +8,7 @@ var moveZeroes = function(nums) {
             nums.push(0)
         }
     }
-    return nums
+   return nums
 };
 console.log(moveZeroes(nums));
 
-
-// let arr=[0,1,0,3,12];
-// var move=function(arr){
-//     for(let i=0;i<arr.length;i++){
-//         if(arr[i]===0){
-//             arr.splice(i,1);
-//             arr.push(0)
-//         }
-//     }
-//     return arr;
-// };
-// console.log(move(arr));
